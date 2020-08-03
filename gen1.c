6395f338c3fe0ee8a8311390df29becb62ffb22e
DIY_Instruments <DIYInstruments@yahoo.com>
2020-08-02 21:42:25 -0400

    updated noise and output format

--- gen1.c
+++ gen1.c
@@ -2,20 +2,49 @@
 #include <math.h>
 #include <stdlib.h>
 
-float Fs       = 1000.0f;
-float neck_var = 0.1f;
+float Fs       = 1000.0f;  // samp freq in Hz
+float neck_var = 0.1f;   // variance of noise
 
-float pos      = 0.0f;
-float vel      = 0.0f;
-float noise    = 0.0f;
- 
+float pos        = 0.0f; // kinetics
+float pos_noise  = 0.0f;
+float vel        = 0.0f;
+float acc        = 0.0f;
+
+float pos1       = 0.0f; // prev step
+float pos_noise1 = 0.0f; 
+float vel1       = 0.0f;
+
+float dpos       = 0.0f; // derivatives
+float dpos_noise = 0.0f;
+float dvel       = 0.0f;
+
+float noise      = 0.0f;
+
+float noise_mag = 1 * 2000;
+
 void out_row( void )
 {
-  noise = ( ( random()%2000 ) - 1000 )/ 1000.0f;
+  noise = ( ( random()%(int32_t)noise_mag ) - noise_mag / 2.0f )/ noise_mag / 2.0f;
+  pos_noise = pos + noise;
 
-  printf( "%f, ", pos );
-  printf( "%f, ", pos + noise );
+  dpos = pos - pos1;
+  dpos_noise = pos_noise - pos_noise1;
+  vel = dpos_noise * Fs;
+  dvel = vel - vel1;
+  acc = dvel * Fs;
+
+  printf( "%.4f, ", pos );
+  printf( "%03.4f, ", pos_noise );
+  printf( "% 3.4f, ", dpos_noise );
+  printf( "% 4.4f, ", vel );
+  printf( "% 4.4f, ", dvel );
+  printf( "% 4.4f, ", acc );
   printf( "\n" );
+
+  // prep for next iter
+  pos1       = pos;
+  pos_noise1 = pos_noise;
+  vel1 = vel;
 }
 
 void out_samps( float samp_len )
